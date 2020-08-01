6285c7854edc4183daa30d16480720f4ca62a604
aswintb <56666817+aswintb@users.noreply.github.com>
2020-08-01 13:47:00 +0530

    Update Auto_Lighting_v1.ino

--- Auto_Lighting_v1.ino
+++ Auto_Lighting_v1.ino
@@ -1,64 +1,86 @@
-int relay_pin=12;
-int ldr_pin=A0;
-int ldr_value = 0;
-int pir_value=LOW;
 int night_limit=800;
-int pir_in=10;
+
+
+int walk_relay_pin = 9;
+
+
+
+int pir_pin = 7; 
+int pirValue; 
+
+int ldr_pin=A0;
+int ldr_value;
+
+
+int buzzer_pin= 8;
 
 
 void setup() 
+{
+  pinMode(walk_relay_pin, OUTPUT);
+  pinMode(buzzer_pin, OUTPUT);
+  
+  pinMode(pir_pin, INPUT);
+  welcome();
+
+}
+ 
 
 
+ 
+void loop() 
 {
 
-Serial.begin(9600);
-pinMode(relay_pin, OUTPUT);
-digitalWrite(relay_pin, HIGH);
-int i;
- /* for( i=0;i<=60;i++)
+
+  
+
+pirValue = digitalRead(pir_pin);
+ldr_value = analogRead(ldr_pin);
+  
+  
+ 
+ 
+if((pirValue == 1)&&(ldr_value>night_limit))
+  
   {
-    delay(1000);
+   digitalWrite(walk_relay_pin, LOW);
+   delay(15000);
+   digitalWrite(walk_relay_pin, HIGH);
+  
+  } 
+  
+  else if((pirValue == 1)&&(ldr_value<night_limit))
+  {
+     buzzer(2,50);
+     delay(9000);
   }
   
   
-  if(i!=0)
-{
-  for(int i=0;i<=3;i++)
- digitalWrite(relay_pin, LOW); 
- delay(100);
- digitalWrite(relay_pin, HIGH);
-  delay(100);
-}
-
-*/
-
-pinMode(pir_in, INPUT);
-
+  
 }
 
 
-void loop() {
-  pir_value = digitalRead(pir_in);
-  ldr_value = analogRead(ldr_pin);
-  Serial.println(ldr_value);
- Serial.println(  pir_value);
-  
 
-  
-  if((ldr_value>night_limit)&&(pir_value== HIGH))
+void  buzzer(int buzzer_loop,int buzzer_duration)
+ {  
+  for(int i=0;i<buzzer_loop;i++)
   {
-  digitalWrite(relay_pin, LOW); 
-digitalWrite(13, HIGH); 
-     Serial.println( "LIght on");
+  digitalWrite(buzzer_pin, HIGH);
+  delay(buzzer_duration); 
+  digitalWrite(buzzer_pin,LOW);
+  delay(buzzer_duration); 
+ 
   }
 
-  else
-  {                 
-    digitalWrite(13, LOW);      
-  digitalWrite(relay_pin, HIGH);    
-   Serial.println( "LIght of");
-  
-  }                       
-delay(500);
+}
 
+
+void welcome()
+{
+  
+  digitalWrite(walk_relay_pin, LOW);
+  buzzer(1,1000);
+  digitalWrite(walk_relay_pin, HIGH);
+  delay(59000);
+  buzzer(2,300);
 }
